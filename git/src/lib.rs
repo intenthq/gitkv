7ff55f05daf5f65bd7ed7057136eadab12a0def9
Albert Pastrana <albert.pastrana@intenthq.com>
2019-03-14 14:26:15 +0100

    Adds tests to `cat_file`
    This is part of #3
    
    Signed-off-by: Albert Pastrana <albert.pastrana@intenthq.com>

--- git/src/lib.rs
+++ git/src/lib.rs
@@ -2,6 +2,8 @@ extern crate git2;
 
 use git2::{Error, Repository};
 
+/// Given an existing git repository, it will read the blob that the reference and the filename
+/// point to and return it as a String.
 pub fn cat_file(repo: &Repository, reference: &str, filename: &str) -> Result<String, Error> {
     let reference = repo.find_reference(reference)?;
 
@@ -12,4 +14,89 @@ pub fn cat_file(repo: &Repository, reference: &str, filename: &str) -> Result<St
     let blob = repo.find_blob(te.id())?;
 
     Ok(String::from_utf8_lossy(blob.content()).to_string())
+}
+
+#[cfg(test)]
+mod tests {
+
+    extern crate tempdir;
+
+    use super::cat_file;
+
+    use git2::Repository;
+    use std::fs;
+    use std::io::Write;
+    use std::path::Path;
+
+    #[test]
+    fn test_cat_file_with_existing_ref_and_file() {
+        with_repo("file content", "dir/existing.file", |repo| {
+            let res = cat_file(repo, "refs/heads/master", "dir/existing.file");
+            assert_eq!(res.ok(), Some(String::from("file content")));
+        })
+    }
+
+    #[test]
+    fn test_cat_file_with_non_existing_ref() {
+        with_repo("file content", "dir/existing.file", |repo| {
+            let res = cat_file(repo, "refs/heads/non-existing", "dir/existing.file")
+                .expect_err("should be an error");
+            assert_eq!(res.code(), git2::ErrorCode::NotFound);
+            assert_eq!(res.class(), git2::ErrorClass::Reference);
+        })
+    }
+
+    #[test]
+    fn test_cat_file_with_non_existing_file() {
+        with_repo("file content", "dir/existing.file", |repo| {
+            let res = cat_file(repo, "refs/heads/master", "non-existing.file")
+                .expect_err("should be an error");
+            assert_eq!(res.code(), git2::ErrorCode::NotFound);
+            assert_eq!(res.class(), git2::ErrorClass::Tree);
+        })
+    }
+
+    #[test]
+    fn test_cat_file_with_dir() {
+        with_repo("content", "dir/existing.file", |repo| {
+            let res = cat_file(repo, "refs/heads/master", "dir").expect_err("should be an error");
+            assert_eq!(res.code(), git2::ErrorCode::NotFound);
+            assert_eq!(res.class(), git2::ErrorClass::Invalid);
+        })
+    }
+
+    pub fn with_repo<F>(file_contents: &str, file: &str, callback: F)
+    where
+        F: Fn(&Repository),
+    {
+        let dir = tempdir::TempDir::new("testgitrepo").expect("can't create tmp dir");
+
+        let repo = Repository::init(&dir).expect("can't initialise repository");
+
+        let path = dir.path().join(file);
+        path.parent().map(|parent| fs::create_dir_all(&parent));
+        fs::File::create(path)
+            .and_then(|mut file| file.write_all(file_contents.as_bytes()))
+            .expect("can't write file contents");
+
+        repo.index()
+            .and_then(|mut index| {
+                index
+                    .add_path(Path::new(file))
+                    .expect("can't add file to index");
+                repo.signature().and_then(|sig| {
+                    index
+                        .write_tree()
+                        .and_then(|tid| repo.find_tree(tid))
+                        .and_then(|tree| {
+                            repo.commit(Some("HEAD"), &sig, &sig, "Initial commit", &tree, &[])
+                        })
+                })
+            })
+            .expect("can't do first commit");
+
+        callback(&repo);
+        dir.close().unwrap();
+    }
+
 }
